04065ebb17e60443ca365fdb8530c573e56408e2
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-29 11:27:49 -0800

    Initial commit.

---
+++ makefile
@@ -0,0 +1,3 @@
+All: prog1.out
+prog1.out: prog1.cpp
+	g++ -std=c++17 -o prog1.out prog1.cpp
---
+++ prog1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+int main(int argc, char** argv){
+	return 0;
+}
