8252ccb5c8d7cfec607697d61beb146eb229bb67
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-29 16:00:36 -0800

    Implement insert recursively.

--- prog1.cpp
+++ prog1.cpp
@@ -14,17 +14,17 @@ struct TreeNode{
 };
 
 void pre_order(struct TreeNode *root){
-	if (root != NULL){
+	if (root != nullptr){
 		std::stack <struct TreeNode *> to_print;
 		to_print.push(root);
 		while (!to_print.empty()){
 			root = to_print.top();
 			to_print.pop();
 			std::cout << root->value << " ";
-			if (root->right_child != NULL){
+			if (root->right_child != nullptr){
 				to_print.push(root->right_child);
 			}
-			if (root->left_child != NULL){
+			if (root->left_child != nullptr){
 				to_print.push(root->left_child);
 			}
 	    }
@@ -35,8 +35,27 @@ void pre_order(struct TreeNode *root){
     std::cout << "\n";
 }
 
+void insert(struct TreeNode *&root, int to_insert){
+	if (root != nullptr){
+		if (to_insert < root->value){
+			insert(root->left_child, to_insert);
+		}
+		if (to_insert > root->value){
+			insert(root->right_child, to_insert);
+		}
+	}
+	else{
+		root = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+		root->value = to_insert;
+		root->left_child = nullptr;
+		root->right_child = nullptr;
+	}
+}
+
 int main(int argc, char** argv){
-	struct TreeNode *new_node; /*(struct TreeNode*) malloc(sizeof(struct TreeNode))*/
+	struct TreeNode *new_node;
+	insert(new_node, 6);
+	insert(new_node, 7);
 	pre_order(new_node);
 	return 0;
 }
